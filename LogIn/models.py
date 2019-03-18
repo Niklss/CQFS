89379ddc58da5aa87c4273b1ad27d1f97c18e06a
niklss <albert_badretdin@mail.ru>
2019-03-19 01:22:10 +0300

    Added checking is user in the system. Added login to the db. Created DBHelper class to use and maintain in db work.

--- LogIn/models.py
+++ LogIn/models.py
@@ -2,21 +2,32 @@ from django.db import models
 
 
 # Create your models here.
+class DBHelper():
+    def add_user(self, args):
+        user = Sys_User.objects.create_user(args)
+        return user
+
+    def is_in_system(self, login, password):
+        user = Sys_User.objects.get(login=login, password=password)
+        if user is not None:
+            return user
+        return False
+
+
 class User_Manager(models.Manager):
+
     def create_user(self, args):
-        sys_user = self.create(first_name=args["first_name"], last_name=args["last_name"], password=args["password"], role=args["role"], group=args["group"])
+        sys_user = self.create(login=args["login"], password=args["password"], first_name=args["first_name"],
+                               last_name=args["last_name"], role=args["role"], group=args["group"])
         return sys_user
 
 
 class Sys_User(models.Model):
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
     role = models.IntegerField()
     group = models.CharField(max_length=50)
 
     objects = User_Manager()
-
-def add_user(args):
-    user = Sys_User.objects.create_user(args)
-    return user
