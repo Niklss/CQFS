5c8214fde000afd7a9f3ef4ae25d475494a3e7c7
niklss <albert_badretdin@mail.ru>
2019-03-19 04:12:14 +0300

    New app, connected urls(almost)

--- LogIn/views.py
+++ LogIn/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request, '/login.html')
+#
+# def contact(request):
+#     return render(request, 'personal/basic.html', {'content':['There is my email:', 'leonor13092000@mail.ru']})
