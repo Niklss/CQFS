671dfc39e532a913c4db066d40d97611c03a5955
niklss <albert_badretdin@mail.ru>
2019-04-01 18:23:55 +0300

    Added SessionAuth

--- LogIn/api.py
+++ LogIn/api.py
@@ -1,14 +1,19 @@
 from tastypie.resources import ModelResource
+from tastypie.authentication import SessionAuthentication
 from .models import SurveyTemplateModel
 from .models import SurveyAnswerModel
+
 
 class SurveyTemplateResource(ModelResource):
     class Meta:
         queryset = SurveyTemplateModel.objects.all()
+        authentication = SessionAuthentication()
         resource_name = 'surveytemplate'
 
 class SurveyAnswerResource(ModelResource):
     class Meta:
         queryset = SurveyAnswerModel.objects.all()
+        queryset = SurveyTemplateModel.objects.create_survey()
+        authentication = SessionAuthentication()
         resource_name = 'surveyanswer'
 
