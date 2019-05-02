09b2fb39cc74a35e61bbe694086b015206969e7b
niklss <albert_badretdin@mail.ru>
2019-05-03 02:47:19 +0300

    Added models for holding courses and groups. Also reworked user page.

--- CQFS/settings.py
+++ CQFS/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '$@tizvchs8d%8c*!9(pb)tm)wh!d3dsdha2_pz91^o65=zw8=n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.109", "127.0.0.1", "10.178.128.37", "0.0.0.0"]
+ALLOWED_HOSTS = ["192.168.1.109", "127.0.0.1", "10.90.138.248", "0.0.0.0"]
 
 # Application definition
 
