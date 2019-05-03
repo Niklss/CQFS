a4ef46eeeb41d4c953ea4eb78502312a1bfe16c6
niklss <albert_badretdin@mail.ru>
2019-05-03 03:22:05 +0300

    Added models for holding courses and groups. Also reworked user page.

--- CQFS/settings.py
+++ CQFS/settings.py
@@ -141,7 +141,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "templates")
+    os.path.join(BASE_DIR, "templates/static"),
+
 ]
